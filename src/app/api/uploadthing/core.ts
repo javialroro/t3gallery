211c636405f7ffe4126d6b4c6e8c2d6cc2031e36
javialroro <102406440+javialroro@users.noreply.github.com>
2025-02-12 17:26:18 -0600

    Uploading permissions

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,10 +1,11 @@
 import { createUploadthing, type FileRouter } from "uploadthing/next";
 import { UploadThingError } from "uploadthing/server";
-import { auth } from "@clerk/nextjs/server";
+import { auth, clerkClient } from "@clerk/nextjs/server";
 import { db } from "~/server/db";
 import { images } from "~/server/db/schema";
 import ratelimit from "~/server/ratelimit";
 const f = createUploadthing();
+const client = await clerkClient();
 
 // FileRouter for your app, can contain multiple FileRoutes
 export const ourFileRouter = {
@@ -29,7 +30,15 @@ export const ourFileRouter = {
         throw new UploadThingError("Unauthorized");
       }
 
+      const fullUserData = client.users.getUser(user.userId!);
+      
+      if((await fullUserData)?.privateMetadata?.["can-upload"] !== "true") {
+        throw new UploadThingError("User does not have permission to upload");
+      }
+      
+
       const { success } = await ratelimit.limit(user.userId!);
+
       if (!success) {
         throw new UploadThingError("Rate limit exceeded");
       }
