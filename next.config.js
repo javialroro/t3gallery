5c7bfc7dd41d12e6db0673d0023f024efc8938bd
javialroro <102406440+javialroro@users.noreply.github.com>
2025-02-12 17:33:46 -0600

    config 1

--- next.config.js
+++ next.config.js
@@ -6,6 +6,9 @@ import "./src/env.js";
 
 /** @type {import("next").NextConfig} */
 const coreConfig = {
+  experimental: {
+    serverActions: {},
+  },
   images: {
     remotePatterns: [{ hostname: "utfs.io" }],
   },
